ce35eb8ee8dafa11871ff60c128706f8d9c9739f
MD. IMRUL KAYES <66476720+emimrulkayes@users.noreply.github.com>
2022-04-05 06:17:53 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-danger'> Excuse me sir, This site update is running...</h1>
+        </div>
+    );
+};
+
+export default Header;
